e253197a9ad5d2b1fc90f7196738fba00facf46b
Skorpi76 <skorpi76@gmail.com>
2017-03-28 18:21:35 -0400

    Initial Commit
    So far, created a player, temp background, camera follows player script,
    player movent script as well as rotate to cursos, so player always
    points at the cursor

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+    GameObject player;
+    bool followPlayer = true;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (followPlayer == true)
+        {
+            CanFollowPlayer();  
+        }
+	}
+
+    void CanFollowPlayer()
+    {
+        Vector3 newPos = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
+        this.transform.position = newPos;
+    }
+
+
+    public void SetFollowPlayer(bool val)
+    {
+        followPlayer = val;
+    }
+}
